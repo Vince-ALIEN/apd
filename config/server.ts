a1762d1b636e877994314fdb0be5212927ad4195
PhiB64 <philippebarbosa64@gmail.com>
2025-09-29 11:41:39 +0200

    fix pb mode strapi

--- config/server.ts
+++ config/server.ts
@@ -1,8 +1,6 @@
-process.env.NODE_ENV = "development";
-
 export default ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  port: parseInt(process.env.PORT, 10) || env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
   },
