6bbbb1be0abb8047d64529097660c4949f2a4544
PhiB64 <philippebarbosa64@gmail.com>
2025-10-14 15:46:06 +0200

    fix content-type contacts

--- frontend/src/components/ContactModal.jsx
+++ frontend/src/components/ContactModal.jsx
@@ -65,7 +65,7 @@ export default function ContactModal({ isOpen, onClose }) {
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(form),
+          body: JSON.stringify({ data: form }),
         }
       );
 
