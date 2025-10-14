63b1f307df462b97e1ceb17cea7534aff00918a4
PhiB64 <philippebarbosa64@gmail.com>
2025-10-14 18:18:49 +0200

    fix email provider

--- config/server.ts
+++ config/server.ts
@@ -4,4 +4,22 @@ export default ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
+  middleware: {
+    settings: {
+      cors: {
+        origin: [
+          "http://localhost:3000",
+          "http://localhost:3001",
+          "http://192.168.1.101:3000",
+          "https://apd-three.vercel.app",
+          "https://eglise-aules.vercel.app",
+          "https://apd-7ov8.onrender.com",
+        ],
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        headers: ["Content-Type", "Authorization", "Origin", "Accept"],
+        credentials: true,
+        maxAge: 31536000,
+      },
+    },
+  },
 });
